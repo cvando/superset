4eb3f3a4f56ced0a26f65785e1ac8608e33c2930
cvando <clement.vandoolaeghe@gmail.com>
2018-02-16 13:12:37 +0100

    Update superset.sh

--- superset.sh
+++ superset.sh
@@ -1,32 +1,11 @@
 # set up Superset
-if [ ! -f /var/lib/superset/.setup-complete ]; then
-  echo "Running first time setup for Superset"
+echo "Running first time setup for Superset"
 
-  echo "Creating admin user"
-  cat > /var/lib/superset/admin.config <<EOF
-admin
-admin
-admin
-admin@admin.fr
-admin
-admin
-EOF
+echo "Initializing database"
+superset db upgrade
 
-  /bin/sh -c '/usr/local/bin/fabmanager create-admin --app superset < /var/lib/superset/admin.config'
-
-  rm /var/lib/superset/admin.config
-
-  echo "Initializing database"
-  superset db upgrade
-
-  echo "Creating default roles and permissions"
-  superset init
-
-  touch /var/lib/superset/.setup-complete
-else
-  # always upgrade the database, running any pending migrations
-  superset db upgrade
-fi
+echo "Creating default roles and permissions"
+superset init
 
 echo "Starting up Superset"
 superset runserver
